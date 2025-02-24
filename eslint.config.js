e847d9fb10781911dc33259898b1f5dd3fb958cc
luanmacea <luanmacea@gmail.com>
2025-02-24 20:13:32 -0300

    estrutura inicial do projeto feita

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import eslint from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tseslintParser from "@typescript-eslint/parser";
+import prettier from "eslint-plugin-prettier";
+
+export default [
+    eslint.configs.recommended,
+    {
+        files: ["**/*.ts", "**/*.tsx"],
+        languageOptions: {
+            parser: tseslintParser,
+        },
+        plugins: {
+            "@typescript-eslint": tseslint,
+            prettier: prettier,
+        },
+        rules: {
+            "prettier/prettier": ["error"],
+            "@typescript-eslint/no-unused-vars": ["warn"],
+            "react/react-in-jsx-scope": "off",
+        },
+    },
+];
