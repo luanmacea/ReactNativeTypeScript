aeedc10a09f304cc66f3b8b1eae33c75c7277562
luanmacea <luan.macea@arista.com.br>
2025-02-25 03:10:47 -0300

    preparacao para navegacao do app

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'expo-router'
 import { View, Text } from 'react-native'
 
 export default function Home() {
   return (
     <View>
       <Text>Página Inicial</Text>
+      <Link href={'/(tabs)/dashboard'}>dashboard</Link>
     </View>
   )
 }
