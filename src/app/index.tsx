7e471f673e113cd8e8ca6c31b32861aae2e7b587
luanmacea2 <luan.macea@arista.com.br>
2025-09-07 17:05:24 -0300

    teste

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -12,6 +12,7 @@ export default function Index() {
     const savedMode = await AsyncStorage.getItem(THEME_KEY)
     if (savedMode === 'light' || savedMode === 'dark') {
       dispatch(setThemeMode(savedMode))
+      console.log('ola')
     }
   }
 
